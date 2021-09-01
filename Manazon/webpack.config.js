bf2d3f17215d5d36773e137acbd806b36f623f9e
anthill499 <anthill499@gmail.com>
2021-09-01 17:09:48 -0400

    first commit after new version

---
+++ Manazon/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./frontend/bench_bnb.jsx",
+  output: {
+    filename: "./bundle.js",
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+
+        exclude: /(node_modules)/,
+
+        use: {
+          loader: "babel-loader",
+
+          options: {
+            presets: ["@babel/env", "@babel/react"],
+          },
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+};
