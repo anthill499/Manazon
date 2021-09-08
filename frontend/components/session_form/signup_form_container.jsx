ec3fe2be84cb480b85285289a1b7c03bd01470b2
anthill499 <anthill499@gmail.com>
2021-09-08 14:21:30 -0400

    fixed sign up form prior

--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -2,13 +2,7 @@ import { connect } from "react-redux";
 import { signup, clearErrors } from "../../actions/session_actions";
 import SignUpForm from "./signup_form";
 
-<<<<<<< HEAD:Manazon/frontend/components/session_form/signup_form_container.jsx
-<<<<<<< HEAD
-=======
-const mapStateToProps = (state) => ({
-=======
 const mapStateToProps = ({ errors }) => ({
->>>>>>> master:frontend/components/session_form/signup_form_container.jsx
   formData: {
     fullName: "",
     username: "",
@@ -17,18 +11,10 @@ const mapStateToProps = ({ errors }) => ({
   },
   errors: errors.session,
 });
-<<<<<<< HEAD:Manazon/frontend/components/session_form/signup_form_container.jsx
 
->>>>>>> auth
-=======
->>>>>>> master:frontend/components/session_form/signup_form_container.jsx
 const mapDispatchToProps = (dispatch) => ({
   processForm: (formUser) => dispatch(signup(formUser)),
   clearErrors: () => dispatch(clearErrors()),
 });
 
-<<<<<<< HEAD
-export default connect(null, mapDispatchToProps)(SignUpForm);
-=======
 export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
->>>>>>> auth
