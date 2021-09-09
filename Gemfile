c5a3eb7615bd38bacd86f4728a4c1fb673342807
anthill499 <anthill499@gmail.com>
2021-09-08 22:23:21 -0400

    finally hid api key

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.3'
 
+gem 'dotenv-rails'
 gem 'faker'
 gem 'rest-client'
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 require 'rest-client'
-require '../.keys.rb'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first);
+
 
 User.destroy_all
 Product.destroy_all
@@ -23,7 +16,7 @@ richard = User.create!(
   username: "rzleu123",
   full_name: "Richard Leung")
 
-rm = RestClient.get "https://amazon-products1.p.rapidapi.com/search?country=US&query=Macbook%2BPro&page=1&rapidapi-key=#{$api_key}"
+rm = RestClient.get "https://amazon-products1.p.rapidapi.com/search?country=US&query=Macbook%2BPro&page=1&rapidapi-key=#{ENV['API_KEY']}"
 rm_array = JSON.parse(rm)["results"]
 User.destroy_all
 Product.destroy_all
@@ -33,6 +26,5 @@ rm_array.each do |product|
     title: product["title"],
     description: "this works",
     price: product["prices"]["current_price"],
-    inventory: product["reviews"]["total_reviews"]  # integer
-  )
+    inventory: product["reviews"]["total_reviews"])
 end
