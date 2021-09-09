b6a78d3db7afa41366c7dfcecfc12b8a17c8ad79
anthill499 <anthill499@gmail.com>
2021-09-08 20:52:30 -0400

    hide keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'rest-client'
+require '../.keys.rb'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -22,7 +23,7 @@ richard = User.create!(
   username: "rzleu123",
   full_name: "Richard Leung")
 
-rm = RestClient.get "https://amazon-products1.p.rapidapi.com/search?country=US&query=Macbook%2BPro&page=1&rapidapi-key=6e683ec48bmsha88dd18ad67aab4p1b3e40jsn51fffefa2529"
+rm = RestClient.get "https://amazon-products1.p.rapidapi.com/search?country=US&query=Macbook%2BPro&page=1&rapidapi-key=#{$api_key}"
 rm_array = JSON.parse(rm)["results"]
 User.destroy_all
 Product.destroy_all
